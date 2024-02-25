1f64a6d9ce9a699a167d054d2547695cab7a4977
Hossein Mohammadi <hossein.mohammadi@ah.nl>
2024-02-25 15:10:41 +0100

    Add service and repository layer

---
+++ repository/src/main/java/com/eightbits/commerce/trade/products/repository/model/ProductDocument.java
@@ -0,0 +1,52 @@
+package com.eightbits.commerce.trade.products.repository.model;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.Indexed;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.Field;
+
+import java.util.UUID;
+
+@Document("products")
+public class ProductDocument {
+
+    @Id @Field("_id")
+    private String id;
+
+    @Indexed
+    private UUID productId;
+    private String name;
+    private String description;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public UUID getProductId() {
+        return productId;
+    }
+
+    public void setProductId(UUID productId) {
+        this.productId = productId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
---
+++ repository/src/test/java/com/eightbits/commerce/trade/products/ProductApplicationTests.kt
@@ -0,0 +1,14 @@
+package com.eightbits.commerce.trade.products
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.ActiveProfiles
+
+@ActiveProfiles("test")
+@SpringBootTest
+class ProductApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+}
